a6b429d1f6247237e7ccd58ea3acecfa2f14e89d
Abdohegazye <pateralax@gmail.com>
2024-12-27 19:13:54 +0200

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -2,13 +2,13 @@ const MIN_COMPRESS_LENGTH = 1024
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100
 
 function shouldCompress(req) {
-  const { originType, originSize, webp } = req.params
+  const { originType, originSize, jpeg } = req.params
 
   if (!originType.startsWith('image')) return false
   if (originSize === 0) return false
-  if (webp && originSize < MIN_COMPRESS_LENGTH) return false
+  if (jpeg && originSize < MIN_COMPRESS_LENGTH) return false
   if (
-    !webp &&
+    !jpeg &&
     (originType.endsWith('png') || originType.endsWith('gif')) &&
     originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
   ) {
